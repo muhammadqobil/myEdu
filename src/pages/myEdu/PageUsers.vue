7acd9955a0c478add75672c33461aa1d90d90748
f_elbek <elbekusmonjonov007@gmail.com>
2023-05-25 17:47:16 +0500

    added some changes

---
+++ src/pages/myEdu/PageUsers.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page padding class="row items-start justify-center">
+    <q-card bordered class="fit">
+      <module-header/>
+      <users/>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import ModuleHeader from "components/base/ModuleHeader";
+import Users from "components/myEdu/Users";
+export default {
+  name: "PageUsers",
+  components: {Users, ModuleHeader}
+}
+</script>
+
+<style scoped>
+
+</style>
